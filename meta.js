94eeb082e4b8e8a5bcd3253e5f98941ca4bf7615
Eduardo San Martin Morote <posva@users.noreply.github.com>
2017-02-07 12:45:00 +0100

    Hide cd project when generating in current dir (#498)

--- meta.js
+++ meta.js
@@ -143,5 +143,5 @@ module.exports = {
     "build/webpack.test.conf.js": "unit",
     "test/e2e/**/*": "e2e"
   },
-  "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
