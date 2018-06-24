shenron/webpack

Automatically install Dependencies and fix lint errors (#1133)
* Batman!

* removed special airbnb template strings since we can auto-fix them now.

* fix string bug

* remove last airbnb template string

* * Make autoinstall optional
* run lint-fix for airbnb AND standard
* print message if user has to manually lint-fix

* webpack.conf.js is not needed in jest and e2e (#1135)

* fix filename of `.eslintignore` (#1136)

* Batman!

* removed special airbnb template strings since we can auto-fix them now.

* fix string bug

* remove last airbnb template string

* * Make autoinstall optional
* run lint-fix for airbnb AND standard
* print message if user has to manually lint-fix

* used chalk to color up console logs

* add option for yarn

* change double quotes to single quotes, remove where possible

* generalize log message

template/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import Router from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import HelloWorld from '@/components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+      component: HelloWorld
+    }
+  ]
+})

sunkarapk/spoiler

template/src/router/index.js
@@ -1,47 +1,47 @@
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import VueRouter from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 {{#auth}}
 
-import auth from '@/auth'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import AuthForbidden from '@/components/AuthForbidden'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import AuthCallback from '@/components/AuthCallback'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import auth from '@/auth'
+import AuthForbidden from '@/components/AuthForbidden'
+import AuthCallback from '@/components/AuthCallback'
 {{/auth}}
 
-import Hello from '@/components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Hello from '@/components/Hello'
 
-Vue.use(VueRouter){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(VueRouter)
 
 const routes = [
   {{#auth}}
   {
     path: '/forbidden',
     name: 'AuthForbidden',
-    component: AuthForbidden{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    component: AuthForbidden
   },
   {
     path: '/callback',
     name: 'AuthCallback',
-    component: AuthCallback{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    component: AuthCallback
   },
   {{/auth}}
   {
-    path: '/',
-    name: 'Hello',
-    component: Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     {{#auth}}
     // Uncomment out the following part to secure this route with authentication
     // meta: {
-    //   auth: true{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    // }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    //   auth: true,
+    // },
     {{/auth}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-]{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  }
+]
 
 /* eslint-disable no-new */
 const router = new VueRouter({
   routes,
-  mode: 'history'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  mode: 'history'
+})
 {{#auth}}
 
 router.beforeEach((to, from, next) => {
@@ -50,14 +50,14 @@ router.beforeEach((to, from, next) => {
       return next({
         name: 'AuthForbidden',
         query: {
-          redirect: to.fullPath{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-        }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-      }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+          redirect: to.fullPath
+        }
+      })
     }
   }
 
-  return next(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  return next()
+})
 {{/auth}}
 
-export default router{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+export default router